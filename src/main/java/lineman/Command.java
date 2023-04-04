168cbaadac8b69d7162081494932ab8320c3ed41
itstotallyjan <jan.santi@icloud.com>
2023-04-04 11:50:30 -0400

    first commit!

---
+++ src/main/java/lineman/Command.java
@@ -0,0 +1,31 @@
+package lineman;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public interface Command {
+    HashMap<String, Command> subcommands = new HashMap<>();
+
+    void run(Lineman lineman, String[] commands);
+
+    default Command addSubcommand(String name, Command command) {
+        subcommands.put(name, command);
+        return this;
+    }
+
+    default void runSubcommand(Lineman lineman, String[] args) {
+        if (subcommands.containsKey(args[0])) {
+            ArrayList<String> cmds = new ArrayList<>();
+            Collections.addAll(cmds, args);
+            cmds.remove(0);
+            subcommands.get(args[0]).run(lineman, cmds.toArray(String[]::new));
+        } else {
+            List<String> predictions = new ArrayList<>();
+            subcommands.forEach((name, command) -> {if (name.startsWith(args[0]) || name.contains(args[0])) predictions.add(name);});
+            lineman.getLogger().severe("unknown command '" + args[0] + "'. did you mean one of, " + predictions.toString().replace("[", "").replace("]", "") + "?");
+        }
+    }
+
+}
---
+++ src/main/java/lineman/Lineman.java
@@ -0,0 +1,67 @@
+package lineman;
+
+import java.util.*;
+
+public class Lineman {
+    private final List<Thread> startupHooks;
+    private final List<Thread> shutdownHooks;
+    private final HashMap<String, Command> commandsRegistry;
+    private final Logger logger;
+
+    private Lineman() {
+        this.startupHooks = new ArrayList<>();
+        this.shutdownHooks = new ArrayList<>();
+        this.commandsRegistry = new HashMap<>();
+        this.logger = new Logger();
+    }
+
+    public static Lineman create() {
+        return new Lineman();
+    }
+
+    public Logger getLogger() { return logger; }
+
+    public void addStartupHook(Thread hook) {
+        startupHooks.add(hook);
+    }
+
+    public void clearStartupHooks() {
+        startupHooks.clear();
+    }
+
+    public void addShutdownHook(Thread hook) {
+        shutdownHooks.add(hook);
+    }
+
+    public void clearShutdownHooks() {
+        shutdownHooks.clear();
+    }
+
+    public Command addCommand(String name, Command command) {
+        commandsRegistry.put(name, command);
+        return command;
+    }
+
+
+    public void run(String[] args) {
+        // run startup hooks
+        for (Thread hook:startupHooks) {
+            hook.start();
+        }
+        Runtime r = Runtime.getRuntime();
+        for (Thread hook:shutdownHooks) {
+            r.addShutdownHook(hook);
+        }
+
+        if (commandsRegistry.containsKey(args[0])) {
+            ArrayList<String> commands = new ArrayList<>();
+            Collections.addAll(commands, args);
+            commands.remove(0);
+            commandsRegistry.get(args[0]).run(this, commands.toArray(String[]::new));
+        } else {
+            List<String> predictions = new ArrayList<>();
+            commandsRegistry.forEach((name, command) -> {if (name.startsWith(args[0]) || name.contains(args[0])) predictions.add(name);});
+            logger.severe("unknown command '" + args[0] + "'. did you mean one of, " + predictions.toString().replace("[", "").replace("]", "") + "?");
+        }
+    }
+}
